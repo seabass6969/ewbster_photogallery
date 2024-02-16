24a0d154857678116b9e8d7469c0c3e3d472ecb0
BuildTools <cephas8080@outlook.com>
2024-02-16 22:01:53 +0000

    with index baby

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { fly } from "svelte/transition";
+export let displayed = "normal"
+export let index = 0
 const download = async (url:string, filename:string) => {
     const data = await fetch(url)
     const blob = await data.blob()
@@ -23,17 +25,26 @@ const download = async (url:string, filename:string) => {
 	export let src: string;
 </script>
 
-<div class="grid">
-	<img {src} alt="ewan goofy" class="image" in:fly />
-	<div class="vertical">
-		<button class="download" on:click={() => download(src, src.replace("./ewan_goofy/", ""))}>
-			<span class="material-symbols-outlined"> download </span>
-		</button>
-		<button class="open" on:click={open}>
-			<span class="material-symbols-outlined"> open_in_new</span>
-		</button>
+{#if displayed == "normal"}
+	<div class="grid">
+		<img {src} alt="ewan goofy" class="image" in:fly />
+		<div class="vertical">
+			<button class="download" on:click={() => download(src, src.replace("./ewan_goofy/", ""))}>
+				<span class="material-symbols-outlined"> download </span>
+			</button>
+			<button class="open" on:click={open}>
+				<span class="material-symbols-outlined"> open_in_new</span>
+			</button>
+		</div>
 	</div>
-</div>
+{:else}
+	<div class="grid">
+		<img {src} alt="ewan goofy" class="image" in:fly />
+		<div class="vertical">
+			<span class="">{index}<span>
+		</div>
+	</div>
+{/if}
 
 <style>
 	.image {
---
+++ src/routes/galleryWithindex/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { image_url } from "$lib/image";
+	import Image from "../../components/Image.svelte";
+
+</script>
+<div class="list">
+    {#each image_url as image, index}
+        <Image src={image} displayed="" index={index}/>
+    {/each}
+</div>
+<style>
+    :global(body){
+        overflow: auto;
+    }
+.list {
+display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  grid-auto-flow: dense;
+  row-gap: 1vh;
+  column-gap: 1vw;
+  width: 100%;
+    /* display: grid;
+    align-items: center;
+    justify-content: center;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  grid-template-rows: repeat(auto-fit, minmax(100px, 1fr)); */
+}
+</style>
